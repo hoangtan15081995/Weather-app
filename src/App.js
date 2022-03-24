fe2dfd34edddfacca5c745d5bdc1425c41b447fc
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-03-24 14:37:56 +0700

    weather app

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const api = {
+    key: "3159956f78bb6b83ac1dfaf02b3946c5",
+    base: "https://api.openweathermap.org/data/2.5/",
+  };
+  const [searchInput, setSearchInput] = useState("");
+  const [searchCity, setSearchCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    if (!searchCity) return;
+    const fetchWeatherData = async () => {
+      setLoading(true);
+      try {
+        const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (response.ok) {
+          setWeatherInfo(
+            `${data.name} ${data.sys.country} ${data.weather[0].description} ${data.main.temp}`
+          );
+          setErrorMessage("");
+        } else {
+          setErrorMessage(data.message);
+        }
+      } catch (error) {
+        setErrorMessage(error.message);
+      }
+      setLoading(false);
+    };
+    fetchWeatherData();
+  }, [searchCity, api.base, api.key]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSearchCity(searchInput);
+  };
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => {
+            setSearchInput(e.target.value);
+          }}
+        ></input>
+        <button>Search</button>
+      </form>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <>
+          {errorMessage ? (
+            <div style={{ color: "red" }}>{errorMessage}</div>
+          ) : (
+            <div>{weatherInfo}</div>
+          )}
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
